66ebe54abb9453072b674e874446ea6ab4731558
aj <113114943+aj045045@users.noreply.github.com>
2023-12-13 14:20:08 +0530

    data

--- client/next.config.js
+++ client/next.config.js
@@ -7,7 +7,8 @@ module.exports = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:5000/assessment-system/:path*'
+                // destination: 'http://127.0.0.1:5000/assessment-system/:path*'
+                destination: process.env.FLASK_API_PATH
             },
         ]
     },
